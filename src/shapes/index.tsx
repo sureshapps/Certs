329f46418c779fd0210811902e081779378d134e
Suresh Kaleyannan <hello@suresh.app>
2025-08-29 08:54:10 +0800

    sureshks

---
+++ src/shapes/index.tsx
@@ -0,0 +1,279 @@
+// stars
+import { Star1 } from "./stars/s_1";
+import { Star2 } from "./stars/s_2";
+import { Star3 } from "./stars/s_3";
+import { Star4 } from "./stars/s_4";
+import { Star5 } from "./stars/s_5";
+import { Star6 } from "./stars/s_6";
+import { Star7 } from "./stars/s_7";
+import { Star8 } from "./stars/s_8";
+import { Star9 } from "./stars/s_9";
+import { Star10 } from "./stars/s_10";
+import { Star11 } from "./stars/s_11";
+import { Star12 } from "./stars/s_12";
+import { Star13 } from "./stars/s_13";
+import { Ellipse1 } from "./ellipses/e_1";
+import { Ellipse2 } from "./ellipses/e_2";
+import { Ellipse3 } from "./ellipses/e_3";
+import { Ellipse4 } from "./ellipses/e_4";
+import { Ellipse5 } from "./ellipses/e_5";
+import { Ellipse6 } from "./ellipses/e_6";
+import { Ellipse7 } from "./ellipses/e_7";
+import { Ellipse8 } from "./ellipses/e_8";
+import { Ellipse9 } from "./ellipses/e_9";
+import { Ellipse10 } from "./ellipses/e_10";
+import { Ellipse11 } from "./ellipses/e_11";
+import { Flower1 } from "./flowers/f_1";
+import { Flower2 } from "./flowers/f_2";
+import { Flower3 } from "./flowers/f_3";
+import { Flower4 } from "./flowers/f_4";
+import { Flower5 } from "./flowers/f_5";
+import { Flower6 } from "./flowers/f_6";
+import { Flower7 } from "./flowers/f_7";
+import { Flower8 } from "./flowers/f_8";
+import { Flower9 } from "./flowers/f_9";
+import { Flower10 } from "./flowers/f_10";
+import { Flower11 } from "./flowers/f_11";
+import { Flower12 } from "./flowers/f_12";
+import { Flower13 } from "./flowers/f_13";
+import { Flower14 } from "./flowers/f_14";
+import { Flower15 } from "./flowers/f_15";
+import { Flower16 } from "./flowers/f_16";
+import { Wheel1 } from "./wheels/w_1";
+import { Wheel2 } from "./wheels/w_2";
+import { Wheel3 } from "./wheels/w_3";
+import { Wheel4 } from "./wheels/w_4";
+import { Wheel5 } from "./wheels/w_5";
+import { Wheel6 } from "./wheels/w_6";
+import { Misc1 } from "./miscs/m_1";
+import { Misc2 } from "./miscs/m_2";
+import { Misc3 } from "./miscs/m_3";
+import { Misc4 } from "./miscs/m_4";
+import { Misc5 } from "./miscs/m_5";
+import { Misc6 } from "./miscs/m_6";
+import { Misc7 } from "./miscs/m_7";
+import { Misc8 } from "./miscs/m_8";
+import { Misc9 } from "./miscs/m_9";
+import { Misc10 } from "./miscs/m_10";
+import { Triangle1 } from "./triangles/t_1";
+import { Triangle2 } from "./triangles/t_2";
+import { Triangle3 } from "./triangles/t_3";
+import { Triangle4 } from "./triangles/t_4";
+import { Triangle5 } from "./triangles/t_5";
+import { Triangle6 } from "./triangles/t_6";
+import { Triangle7 } from "./triangles/t_7";
+import { Triangle8 } from "./triangles/t_8";
+import { Triangle9 } from "./triangles/t_9";
+import { Triangle10 } from "./triangles/t_10";
+import { Triangle11 } from "./triangles/t_11";
+import { Triangle12 } from "./triangles/t_12";
+import { Triangle13 } from "./triangles/t_13";
+import { Triangle14 } from "./triangles/t_14";
+import { Moon1 } from "./moons/m_1";
+import { Moon2 } from "./moons/m_2";
+import { Moon3 } from "./moons/m_3";
+import { Moon4 } from "./moons/m_4";
+import { Moon5 } from "./moons/m_5";
+import { Moon6 } from "./moons/m_6";
+import { Moon7 } from "./moons/m_7";
+import { Moon8 } from "./moons/m_8";
+import { Moon9 } from "./moons/m_9";
+import { Moon10 } from "./moons/m_10";
+import { Moon11 } from "./moons/m_11";
+import { Moon12 } from "./moons/m_12";
+import { Moon13 } from "./moons/m_13";
+import { Moon14 } from "./moons/m_14";
+import { Moon15 } from "./moons/m_15";
+
+import { ShapeType } from "../lib";
+import { Polygon1 } from "./polygons/p_1";
+import { Polygon2 } from "./polygons/p_2";
+import { Polygon3 } from "./polygons/p_3";
+import { Polygon4 } from "./polygons/p_4";
+import { Polygon5 } from "./polygons/p_5";
+import { Rectangle1 } from "./rectangles/r_1";
+import { Rectangle2 } from "./rectangles/r_2";
+import { Rectangle3 } from "./rectangles/r_3";
+import { Rectangle4 } from "./rectangles/r_4";
+import { Rectangle5 } from "./rectangles/r_5";
+import { Rectangle6 } from "./rectangles/r_6";
+import { Rectangle7 } from "./rectangles/r_7";
+import { Polygon6 } from "./polygons/p_6";
+import { Polygon7 } from "./polygons/p_7";
+import { Polygon8 } from "./polygons/p_8";
+import { Rectangle8 } from "./rectangles/r_8";
+import { Rectangle9 } from "./rectangles/r_9";
+import { Misc11 } from "./miscs/m_11";
+import { Ellipse12 } from "./ellipses/e_12";
+import { Wheel7 } from "./wheels/w_7";
+import { Number0 } from "./numbers/n_0";
+import { Number1 } from "./numbers/n_1";
+import { Number2 } from "./numbers/n_2";
+import { Number3 } from "./numbers/n_3";
+import { Number4 } from "./numbers/n_4";
+import { Number5 } from "./numbers/n_5";
+import { Number6 } from "./numbers/n_6";
+import { Number7 } from "./numbers/n_7";
+import { Number8 } from "./numbers/n_8";
+import { Number9 } from "./numbers/n_9";
+
+const shapes = {
+  star: [
+    Star1,
+    Star2,
+    Star3,
+    Star4,
+    Star5,
+    Star6,
+    Star7,
+    Star8,
+    Star9,
+    Star10,
+    Star11,
+    Star12,
+    Star13,
+  ],
+  flower: [
+    Flower1,
+    Flower2,
+    Flower3,
+    Flower4,
+    Flower5,
+    Flower6,
+    Flower7,
+    Flower8,
+    Flower9,
+    Flower10,
+    Flower11,
+    Flower12,
+    Flower13,
+    Flower14,
+    Flower15,
+    Flower16,
+  ],
+  ellipse: [
+    Ellipse1,
+    Ellipse2,
+    Ellipse3,
+    Ellipse4,
+    Ellipse5,
+    Ellipse6,
+    Ellipse7,
+    Ellipse8,
+    Ellipse9,
+    Ellipse10,
+    Ellipse11,
+    Ellipse12,
+  ],
+  wheel: [Wheel1, Wheel2, Wheel3, Wheel4, Wheel5, Wheel6, Wheel7],
+  moon: [
+    Moon1,
+    Moon2,
+    Moon3,
+    Moon4,
+    Moon5,
+    Moon6,
+    Moon7,
+    Moon8,
+    Moon9,
+    Moon10,
+    Moon11,
+    Moon12,
+    Moon13,
+    Moon14,
+    Moon15,
+  ],
+  misc: [
+    Misc1,
+    Misc2,
+    Misc3,
+    Misc4,
+    Misc5,
+    Misc6,
+    Misc7,
+    Misc8,
+    Misc9,
+    Misc10,
+    Misc11,
+  ],
+  triangle: [
+    Triangle1,
+    Triangle2,
+    Triangle3,
+    Triangle4,
+    Triangle5,
+    Triangle6,
+    Triangle7,
+    Triangle8,
+    Triangle9,
+    Triangle10,
+    Triangle11,
+    Triangle12,
+    Triangle13,
+    Triangle14,
+  ],
+  polygon: [
+    Polygon1,
+    Polygon2,
+    Polygon3,
+    Polygon4,
+    Polygon5,
+    Polygon6,
+    Polygon7,
+    Polygon8,
+  ],
+  rectangle: [
+    Rectangle1,
+    Rectangle2,
+    Rectangle3,
+    Rectangle4,
+    Rectangle5,
+    Rectangle6,
+    Rectangle7,
+    Rectangle8,
+    Rectangle9,
+  ],
+  number: [
+    Number0,
+    Number1,
+    Number2,
+    Number3,
+    Number4,
+    Number5,
+    Number6,
+    Number7,
+    Number8,
+    Number9,
+  ],
+};
+
+// used to autocomplete types
+export type shapeTypes = keyof typeof shapes;
+
+export type componentId = { shapeType: shapeTypes; index: number };
+// return random shape from the shapes list
+export const getRandomShape = ({
+  type,
+  onlyId,
+}: {
+  type?: keyof typeof shapes;
+  onlyId?: boolean;
+} = {}): ShapeType | componentId => {
+  const shapeKeys = Object.keys(shapes);
+  const shapeType: keyof typeof shapes =
+    type ||
+    (shapeKeys[
+      Math.floor(Math.random() * shapeKeys.length)
+    ] as keyof typeof shapes);
+
+  const randomIndex = Math.floor(Math.random() * shapes[shapeType].length);
+  if (onlyId) {
+    return {
+      shapeType,
+      index: randomIndex,
+    };
+  }
+  return shapes[shapeType][randomIndex];
+};
+
+export default shapes;
